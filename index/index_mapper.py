f65cbbf86d5f2c75fe3792a1062a07b55f1d812a
Bill Chickering <chickering@cs.stanford.edu>
2013-04-12 03:03:26 -0700

    Implemented inverted index generator mapReduce scripts.

---
+++ index/index_mapper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import sys
+import fileinput
+import json
+import unicodedata
+
+def normalizeQuery(rawquery):
+    return ';'.join(sorted(unicodedata.normalize('NFKD', rawquery)\
+                           .encode('ascii','ignore').lower().split()))
+
+def main():
+    rawqueryid_dict = {}
+    lineNum = 0
+    jsonErrors = 0
+    unicodeErrors = 0
+    rawqueryid = 0
+    last_line = None
+    for line in sys.stdin:
+        lineNum += 1
+        if last_line == line:
+            continue
+        else:
+            last_line = line
+        try:
+            record = json.loads(line)
+            rawquery = record['rawquery']
+            shownitems = record['shownitems']
+        except:
+            jsonErrors += 1
+            continue
+        try:
+            normQuery = normalizeQuery(rawquery)
+        except:
+            unicodeErrors += 1
+            continue
+        if normQuery not in rawqueryid_dict:
+            rawqueryid_dict[normQuery] = rawqueryid
+            rawqueryid += 1
+        for itemid in shownitems:
+            print '%d\t%d'%(itemid, rawqueryid_dict[normQuery])
+
+if __name__ == '__main__':
+  main()
