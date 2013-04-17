fb17bfb94de98e6f37a3969c35a1d2394d874ef8
Bill Chickering <chickering@cs.stanford.edu>
2013-04-16 22:21:04 -0700

    Simplified import scripts since they now will use "filtered" data.

--- scripts/createItemClickImport.py
+++ scripts/createItemClickImport.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # To execute, enter something like:
-# python createItemClickImport.py 000000_0 > 000000_0_itemclick.csv 2> 000000_0_itemclick.err
+# python createItemClickImport.py 000000_0_filt.json > 000000_0_itemclick.csv
 
 import sys
 import fileinput
@@ -9,58 +9,43 @@ import json
 # Locally defined modules
 import encodeField
 
-# GLOBALS
-lineNum = 0
-queryid = 0
-numErrors = 0
-
 def str2bool(s):
     return s.lower() == 'true'
 
 def main():
-    global lineNum
-    global queryid
-    global numErrors
-    last_line = None
+    lineNum = 0
+    queryid = 0
     last_visitorid = None
     last_wmsessionid = None
     last_rawquery = None
     query_shownitems = None
     for line in fileinput.input(sys.argv[1]):
         lineNum = lineNum + 1
-        if last_line == line:
-            continue
-        else:
-            last_line = line
-        try:
-            query = json.loads(line)
-            if last_visitorid != query['visitorid'] or \
-               last_wmsessionid != query['wmsessionid'] or \
-               last_rawquery != query['rawquery']:
-                queryid = queryid + 1
-                last_visitorid = query['visitorid']
-                last_wmsessionid = query['wmsessionid']
-                last_rawquery = query['rawquery']
-                query_shownitems = query['shownitems']
-            for itemid in query['shownitems']:
-                if itemid not in query_shownitems:
-                    query_shownitems.append(itemid)
-            for click in query['clicks']:
-                if click['Position'] == "-1":
-                    queryPosition = -1
-                else:
-                    queryPosition = query_shownitems.index(int(click['ItemId']))
-                output = encodeField.encode(queryid) + ',' + \
-                         encodeField.encode(lineNum) + ',' + \
-                         encodeField.encode(click['ItemId']) + ',' + \
-                         encodeField.encode(str2bool(click['Ordered'])) + ',' + \
-                         encodeField.encode(str2bool(click['InCart'])) + ',' + \
-                         encodeField.encode(queryPosition) + ',' + \
-                         encodeField.encode(click['Position'])
-                print output
-        except:
-            numErrors = numErrors + 1
-            sys.stderr.write(line)
+        query = json.loads(line)
+        if last_visitorid != query['visitorid'] or \
+           last_wmsessionid != query['wmsessionid'] or \
+           last_rawquery != query['rawquery']:
+            queryid = queryid + 1
+            last_visitorid = query['visitorid']
+            last_wmsessionid = query['wmsessionid']
+            last_rawquery = query['rawquery']
+            query_shownitems = query['shownitems']
+        for itemid in query['shownitems']:
+            if itemid not in query_shownitems:
+                query_shownitems.append(itemid)
+        for click in query['clicks']:
+            if click['Position'] == "-1":
+                queryPosition = -1
+            else:
+                queryPosition = query_shownitems.index(int(click['ItemId']))
+            output = encodeField.encode(queryid) + ',' + \
+                     encodeField.encode(lineNum) + ',' + \
+                     encodeField.encode(click['ItemId']) + ',' + \
+                     encodeField.encode(str2bool(click['Ordered'])) + ',' + \
+                     encodeField.encode(str2bool(click['InCart'])) + ',' + \
+                     encodeField.encode(queryPosition) + ',' + \
+                     encodeField.encode(click['Position'])
+            print output
 
 if __name__ == '__main__':
     main()
