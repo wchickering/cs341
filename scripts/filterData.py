05d0387ec76970747e7b198b3e8964b60d7b3e95
Bill Chickering <chickering@cs.stanford.edu>
2013-04-19 00:10:46 -0700

    Modified filterData.py to exclude data with missing visitorids, missing rawqueries, no shownitems, etc.

--- scripts/filterData.py
+++ scripts/filterData.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # To execute, enter something like:
-# python filterData.py 000000_0 > 000000_0_filt.json
+# python filterData.py < 000000_0 > 000000_0_filt.json
 
 import sys
-import fileinput
 import json
 
 def main():
@@ -11,8 +10,9 @@ def main():
     last_line = None
     duplicates = 0
     parseErrors = 0
+    dataErrors = 0
     #constrainedQueries = 0
-    for line in fileinput.input(sys.argv[1]):
+    for line in sys.stdin:
         lineNum = lineNum + 1
         if last_line == line:
             # skip duplicates
@@ -27,6 +27,25 @@ def main():
             # skip malformed json 
             parseErrors += 1
             continue
+        try:
+            visitorid = int(record['visitorid'])
+            assert(visitorid > 0)
+            clickeditems = record['clickeditems']
+            clicks = record['clicks']
+            searchtimestamp = int(record['searchtimestamp'])
+            assert(searchtimestamp > 0)
+            searchattributes = record['searchattributes']
+            shownitems = record['shownitems']
+            assert(len(shownitems) > 0)
+            shownitems_int = [int(item) for item in shownitems]
+            wmsessionid = record['wmsessionid']
+            assert(wmsessionid != '')
+            rawquery = record['rawquery']
+            assert(rawquery != '')
+        except:
+            # skip missing data/format errors
+            dataErrors += 1
+            continue
         #if len(search_attributes) != 1 or\
         #   'search_constraint' not in search_attributes or\
         #   search_attributes['search_constraint'] != '0':
@@ -37,7 +56,8 @@ def main():
         sys.stdout.write(line)
     # display statistics
     print >> sys.stderr, 'duplicates = ' + str(duplicates) +\
-                       ', parseErrors = ' + str(parseErrors) #+\
+                       ', parseErrors = ' + str(parseErrors) +\
+                       ', dataErrors = ' + str(dataErrors) #+\
                        #', constrainedQueries = ' + str(constrainedQueries)
 
 if __name__ == '__main__':
