c3685a58d6084beaa51ed96d7452bcb77547730e
Charles Celerier <charles.celerier@gmail.com>
2013-04-18 18:15:24 -0700

    fixed print bug.

--- scripts/testGenMapper.py
+++ scripts/testGenMapper.py
@@ -5,21 +5,17 @@ import fileinput
 import json
 
 def main():
-  jsonErrors = 0
-  for line in fileinput.input(sys.argv[1]):
-    try:
-      record = json.loads(line)
-      visitorid = record['visitorid']
-      sessionid = record['wmsessionid']
-      rawquery = record['rawquery']
-      timestamp = record['searchtimestamp']
-      shownitems = record['shownitems']
-      clickeditems = record['clickeditems']
-      sep = '\t'
-      print visitorid , sep, sessionid, sep, rawquery, sep, timestamp, sep, shownitems, sep, clickeditems
-    except:
-      jsonErrors = jsonErrors + 1
-      continue
+    jsonErrors = 0
+    for line in fileinput.input(sys.argv[1]):
+        record = json.loads(line)
+        visitorid = record['visitorid']
+        sessionid = record['wmsessionid']
+        rawquery = record['rawquery']
+        timestamp = record['searchtimestamp']
+        shownitems = str(record['shownitems'])
+        clickeditems = str(record['clickeditems'])
+        sep = '\t'
+        print sep.join([visitorid, sessionid, rawquery, timestamp, shownitems, clickeditems])
 
 if __name__ == '__main__':
   main()
