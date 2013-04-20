6a725082733ccf18c31450209c20df6c0a8aa56e
Bill Chickering <chickering@cs.stanford.edu>
2013-04-19 21:29:51 -0700

    Changed index_query.get_posting() to return an empty list if itemid not found.

--- programs/index_query.py
+++ programs/index_query.py
@@ -11,7 +11,7 @@ def get_posting_dict(posting_dict_f):
 
 def get_posting(index_f, posting_dict, itemid):
     if itemid not in posting_dict:
-        return None
+        return []
     index_f.seek(posting_dict[itemid])
     idx, plist = index_f.readline().split('\t')
     assert(idx == str(itemid))
