183301fd87ecfa83308c70aa7915a6f788c2550e
Bill Chickering <chickering@cs.stanford.edu>
2013-04-19 19:06:35 -0700

    Fixed bug in index_test.py.

--- programs/index_test.py
+++ programs/index_test.py
@@ -11,7 +11,7 @@ def main():
         print 'usage: %s <corpus.index> <posting.dict> itemid' % sys.argv[0]
         os._exit(-1)
     index_f = open(sys.argv[1])
-    posting_dict_f = open(sys.argv.[2])
+    posting_dict_f = open(sys.argv[2])
     posting_dict = index_query.get_posting_dict(posting_dict_f)
     uniqueQueryIds = index_query.get_posting(index_f, posting_dict, sys.argv[3])
     print str(uniqueQueryIds)
