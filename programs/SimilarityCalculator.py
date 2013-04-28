b1cc961369be03c32b13221f707a5d4d2c3aa197
Bill Chickering <chickering@cs.stanford.edu>
2013-04-27 19:48:52 -0700

    Major changes to reRank.py. Implemented SimilarityCalculator.py. Removed obsolete programs.

---
+++ programs/SimilarityCalculator.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+__author__ = """Charles Celerier <cceleri@cs.stanford.edu>,
+              Bill Chickering <bchick@cs.stanford.edu>,
+              Jamie Irvine <jirvine@cs.stanford.edu>"""
+
+__date__ = """13 April 2013"""
+
+import sys
+
+# local modules
+import index_query as idx
+
+class SimilarityCalculator:
+
+    # params
+    _score_dump_separator = '\t'
+    _posting_cache_size = 100
+
+    # stats
+    _posting_cache_hits = 0
+    _posting_cache_misses = 0
+    _score_cache_hits = 0
+    _score_cache_misses = 0
+    
+    def __init__(self, index_fname, posting_dict_fname, score_dict_fname=None, \
+                 score_dump_fname=None, verbose=False):
+
+        # misc options
+        self.verbose = verbose
+
+        # posting index
+        self.index_fd = open(index_fname)
+        dict_fd = open(posting_dict_fname)
+        self.posting_dict = idx.get_posting_dict(dict_fd)
+        self.posting_cache = {}
+        self.posting_cache_queue = []
+
+        # score dictionary
+        self.score_dump_fname = score_dump_fname
+        self.score_dict = {}
+        self.score_dict_from_file = False
+        if score_dict_fname:
+            if self.verbose:
+                print >> sys.stderr, 'Uploading scores from ' + \
+                                      score_dict_fname + ' . . .'
+            self.score_dict_from_file = True
+            score_dict_f = open(score_dict_fname)
+            for line in score_dict_f:
+                fields = line.rstrip().split(self._score_dump_separator)
+                self.score_dict[(int(fields[0]), int(fields[1]))] = float(fields[2])
+            if self.verbose:
+                print >> sys.stderr, 'Uploaded ' + str(len(self.score_dict)) + \
+                                     ' scores.'
+
+    def __del__(self):
+        if self.verbose:
+            if not self.score_dict_from_file:
+                print >> sys.stderr, 'SimilarityCalculator cache stats:\n' + \
+                    '\tposting cache hits: ' + str(self._posting_cache_hits) + \
+                    '\tposting cache misses: ' + str(self._posting_cache_misses)
+                if self.score_dump_fname:
+                    print >> sys.stderr, \
+                        '\tscore cache hits: ' + str(self._score_cache_hits) + \
+                        '\tscore cache misses: ' + str(self._score_cache_misses)
+            
+        if self.score_dump_fname:
+            print >> sys.stderr, 'Dumping similarity scores to \'%s\'. . .' % \
+                                 self.score_dump_fname
+            score_dict_f = open(self.score_dump_fname, 'w')
+            for (itemid1, itemid2) in self.score_dict:
+                print >> score_dict_f, self._score_dump_separator.join(\
+                         [itemid1, itemid2, str(self.score_dict[(itemid1, itemid2)])])
+
+    # handles caching
+    def get_posting(self, itemid):
+        if itemid in self.posting_cache:
+            self._posting_cache_hits += 1
+            self.posting_cache_queue.remove(itemid)
+        else:
+            self._posting_cache_misses += 1
+            self.posting_cache[itemid] = \
+                 idx.get_posting(self.index_fd, self.posting_dict, itemid)
+            if len(self.posting_cache) > self._posting_cache_size:
+                del self.posting_cache[self.posting_cache_queue.pop()]
+        self.posting_cache_queue.insert(0, itemid)
+        return self.posting_cache[itemid]
+
+    def similarity(self, itemid1, itemid2):
+        if self.score_dict_from_file:
+            if (min(itemid1, itemid2), max(itemid1, itemid2)) in self.score_dict:
+                return self.score_dict[(min(itemid1, itemid2), max(itemid1, itemid2))]
+            else:
+                return 0.0
+        elif self.score_dump_fname:
+            if (min(itemid1, itemid2), max(itemid1, itemid2)) in self.score_dict:
+                self._score_cache_hits += 1
+                return self.score_dict[(min(itemid1, itemid2), max(itemid1, itemid2))]
+            else:
+                self._score_cache_misses += 1
+                score = self.jaccard(self.get_posting(itemid1), self.get_posting(itemid2))
+                if score > 0.0:
+                    self.score_dict[(min(itemid1, itemid2), max(itemid1, itemid2))] = score
+                return score
+        else:
+            return self.jaccard(self.get_posting(itemid1), self.get_posting(itemid2))
+
+    def jaccard(self, l1, l2):
+        interSize = 0
+        unionSize = 0
+        i = 0
+        j = 0
+        while i < len(l1) and j < len(l2):
+            if l1[i] == l2[j]:
+                interSize += 1
+                unionSize += 1
+                i += 1
+                j += 1
+                continue
+            if l1[i] > l2[j]:
+                unionSize += 1
+                j += 1
+                continue
+            else:
+                unionSize += 1
+                i += 1
+                continue
+        while i < len(l1):
+            unionSize += 1
+            i += 1
+        while j < len(l2):
+            unionSize += 1
+            j += 1
+        if unionSize == 0:
+            return 0.0
+        else:
+            return float(interSize)/unionSize
+              
--- programs/reRank.py
+++ programs/reRank.py
@@ -11,11 +11,11 @@ __author__ = """Charles Celerier <cceleri@cs.stanford.edu>,
 __date__ = """16 April 2013"""
 
 import sys
-import re, json
+import json
 import heapq
 
 # import local modules
-import Similarity as sim
+from SimilarityCalculator import SimilarityCalculator
 from Query import Query
 
 # global stats
@@ -23,64 +23,99 @@ num_reranks = 0
 num_shown_items = 0
 num_nonzero_scores = 0
 
-class NoRerankException(Exception):
-    def __init__(self):
-        pass
-    def __str__(self):
-        return ""
+def parseArgs():
+    from optparse import OptionParser, OptionGroup, HelpFormatter
+    
+    usage = "usage: %prog [options] "\
+            + "--index <index filename> "\
+            + "--dict <dictionary filename> " \
+            + "[-k N] "\
+            + "[--score_dict <score dictionary upload filename> " \
+            + "[--score_dump <score dictionary dump filename> " \
+            + "[--verbose] " \
+            + "<filename>"
+
+    parser = OptionParser(usage=usage)
+    helpFormatter = HelpFormatter(indent_increment=2,\
+                                  max_help_position=10,\
+                                  width=80,\
+                                  short_first=1)
+
+    verboseGroup = OptionGroup(parser, "Verbose")
+    verboseGroup.add_option("-v", "--verbose", action="store_true",\
+                            dest="verbose")
+    verboseGroup.add_option("-q", "--quiet", action="store_false",\
+                            dest="verbose")
+    parser.add_option_group(verboseGroup)
+
+    fileGroup = OptionGroup(parser, "Index options")
+    fileGroup.add_option("--index", dest="indexFn", help="index filename")
+    parser.add_option_group(fileGroup)
+    fileGroup.add_option("--dict", dest="dictionaryFn", help="dictionary filename")
+    parser.add_option_group(fileGroup)
+
+    rankGroup = OptionGroup(parser, "Ranking options")
+    rankGroup.add_option("-k", dest="k", help="re-rank top k items")
+    parser.add_option_group(rankGroup)
+
+    scoreGroup = OptionGroup(parser, "Score options")
+    scoreGroup.add_option("--score_dict", dest="score_dict_fname", \
+                          help="score dictionary upload filename")
+    scoreGroup.add_option("--score_dump", dest="score_dump_fname", \
+                          help="score dictionary dump filename")
+    parser.add_option_group(scoreGroup)
+
+    parser.set_defaults(verbose=False, k=1, indexFn=None, dictionaryFn=None,\
+                        score_dict_fname=None, score_dump_fname=None)
+
+    (options, args) = parser.parse_args()
+
+    if (len(args) > 1):
+        parser.print_usage()
+        sys.exit()
+
+    if ((not options.indexFn) or (not options.dictionaryFn)):
+        parser.print_usage()
+        sys.exit()
+
+    return (options, args)
 
 def printStats():
     print >> sys.stderr, 'num reranks = ' + str(num_reranks)
     print >> sys.stderr, 'num_shown_items = ' + str(num_shown_items)
     print >> sys.stderr, 'num_nonzero_scores = ' + str(num_nonzero_scores)
 
-def reorderShownItems(simCalc, query, options):
+def getTopScoresHeap(simCalc, query, options):
     global num_reranks
     global num_shown_items
     global num_nonzero_scores
-
-    # Retrieve queryLists for previously clicked items
-    #prevQueryLists = []
-    #for previouslyClickedItem in query.previously_clicked_items:
-    #    prevQueryLists.append(idx.get_posting(indexFd, posting_dict, str(previouslyClickedItem)))
-    #if prevQueryLists == []:
-    #    raise NoRerankException
-    #    return query.shown_items
 
     # Determine the top k scores 
     top_scores_heap = []
     for i in range(len(query.shown_items)):
         shownItem = query.shown_items[i]
         num_shown_items += 1
-        #shownItemQueryIds = idx.get_posting(indexFd, posting_dict, str(shownItem))
         score = 0
         for j in range(len(query.previously_clicked_items)):
             # ignore previously clicked items themselves
             if query.previously_clicked_items[j] == shownItem:
                 score = 0
                 break
-            #score += sim.jaccard(prevQueryLists[j], shownItemQueryIds)
-            score += simCalc.similarity(str(query.previously_clicked_items[j]), \
-                                        str(query.shown_items[i]))
+            score += simCalc.similarity(query.previously_clicked_items[j], \
+                                        query.shown_items[i])
         if score > 0:
             num_nonzero_scores += 1
             heapq.heappush(top_scores_heap, (score, i))
             if len(top_scores_heap) > int(options.k):
                 heapq.heappop(top_scores_heap)
-    if (len(top_scores_heap) == 0):
-        raise NoRerankException
+    return top_scores_heap
+
+def reRankItems(query, top_scores_heap):
+    if len(top_scores_heap) == 0:
         return query.shown_items
-
-    num_reranks += len(top_scores_heap)
-
-    # transfer top k to simple list
-    num_reranked = len(top_scores_heap)
     top_scores = sorted(top_scores_heap, key=lambda tup: tup[0], reverse=True)
     top_score_idxs = sorted(top_scores_heap, key=lambda tup: tup[1])
-    
-    i = 0
-    j = 0
-    k = 0
+    i = j = k = 0
     reranked_items = []
     while i < len(query.shown_items):
         if i < len(top_scores):
@@ -110,86 +145,51 @@ def reorderShownItems(simCalc, query, options):
     return reranked_items
 
 def main():
-    from optparse import OptionParser, OptionGroup, HelpFormatter
-    
-    usage = "usage: %prog [options] "\
-            + "-k N "\
-            + "--index <index filename> "\
-            + "--dict <dictionary filename> " \
-            + "<filename>"
-    parser = OptionParser(usage=usage)
-    helpFormatter = HelpFormatter(indent_increment=2,\
-                                  max_help_position=10,\
-                                  width=80,\
-                                  short_first=1)
+    global num_reranks
 
-    verboseGroup = OptionGroup(parser, "Verbose")
-    verboseGroup.add_option("-v", "--verbose", action="store_true",\
-                            dest="verbose")
-    verboseGroup.add_option("-q", "--quiet", action="store_false",\
-                            dest="verbose")
-    verboseGroup.add_option("-m", "--markReordered", action="store_true",\
-                            dest="markReordered")
-    parser.add_option_group(verboseGroup)
-
-    rankGroup = OptionGroup(parser, "Ranking options")
-    rankGroup.add_option("-k", dest="k", help="re-rank top k items")
-    parser.add_option_group(rankGroup)
-
-    fileGroup = OptionGroup(parser, "Index options")
-    fileGroup.add_option("--index", dest="indexFn", help="index filename")
-    parser.add_option_group(fileGroup)
-    fileGroup.add_option("--dict", dest="dictionaryFn", help="dictionary filename")
-    parser.add_option_group(fileGroup)
-
-    parser.set_defaults(verbose=False, k=1, indexFn=None, dictionaryFn=None,\
-                        markReordered=False)
-
-    (options, args) = parser.parse_args()
-
-    if (len(args) == 0):
-        inputFile = sys.stdin
-    elif (len(args) == 1):
+    (options, args) = parseArgs()
+    if len(args) == 1:
         inputFile = open(args[0])
     else:
-        parser.print_usage()
-        sys.exit()
+        inputFile = sys.stdin
 
-    if ((not options.indexFn) or (not options.dictionaryFn)):
-        parser.print_usage()
-        sys.exit()
-
-    simCalc = sim.SimilarityCalculator(options.indexFn, options.dictionaryFn, \
-                                       'simcalc.posting_cache.json', \
-                                       'simcalc.posting_cache_queue.json')
+    # Instantiate Similarity Calculator
+    simCalc = SimilarityCalculator(options.indexFn, options.dictionaryFn, \
+                                   score_dict_fname=options.score_dict_fname, \
+                                   score_dump_fname=options.score_dump_fname, \
+                                   verbose=options.verbose)
 
     try:
         for line in inputFile:
+            # Instantiate query object
             query = Query(line)
+
+            # Compute top scores
+            top_scores_heap = getTopScoresHeap(simCalc, query, options)
+            if options.verbose and len(top_scores_heap) == 0:
+                print >> sys.stderr, \
+                    'Warning: No items re-ranked for:\n' + str(query)
+            num_reranks += len(top_scores_heap)
+
+            # re-rank shown items
+            reordered_shown_items = reRankItems(query, top_scores_heap)
+
+            # write output
             output = {}
             output['visitorid'] = query.visitorid
             output['wmsessionid'] = query.wmsessionid
             output['rawquery'] = query.rawquery
             output['searchattributes'] = query.searchattributes
             output['shown_items'] = query.shown_items
-            try:
-                output['reordered_shown_items'] =\
-                    reorderShownItems(simCalc, query, options)
-            except NoRerankException:
-                print >>sys.stderr, "NoRerankException caught"
-                print >>sys.stderr, line
-                raise
             output['clicked_shown_items'] = query.clicked_shown_items
-            if (options.markReordered):
-                if (output['shown_items'] != output['reordered_shown_items']):
-                    print '*',
+            output['reordered_shown_items'] = reordered_shown_items
             print json.dumps(output)
     except:
         printStats()
         raise
 
-    printStats()
-    sys.exit()
+    if options.verbose:
+        printStats()
 
 if __name__ == '__main__':
     main()
