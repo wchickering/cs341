0003d06da31f4150516d066e348ae8a703ee3b66
Bill Chickering <chickering@cs.stanford.edu>
2013-05-13 20:56:46 -0700

    Added reRank.sh as wrapper script for reRank.py.

---
+++ programs/reRank.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# set to 'dump' or 'load'
+MODE=load
+
+RAWDATA=000050_0
+INDEX=4chunk
+K=3
+COEFF_QUERIES=0.26
+COEFF_CLICKS=1.00
+
+# Assumes WCODE already defined and exported
+PROGRAMS=${WCODE}/programs
+RERANK=${PROGRAMS}/reRank.py
+EVALUATE=${PROGRAMS}/evaluate.py
+DATA=${WCODE}/data
+INPUT=${DATA}/${RAWDATA}.test_data.${INDEX}.filtered
+RERANK_OUTPUT=${DATA}/${RAWDATA}.${INDEX}.k${K}.reordered
+EVAL_OUTPUT=${DATA}/${RAWDATA}.${INDEX}.k${K}.eval
+INDEX_QUERIES=${DATA}/${INDEX}.queries.index
+DICT_QUERIES=${DATA}/${INDEX}.queries.posting.dict
+INDEX_CLICKS=${DATA}/${INDEX}.clicks.index
+DICT_CLICKS=${DATA}/${INDEX}.clicks.posting.dict
+SCORES_QUERIES=${DATA}/${RAWDATA}.${INDEX}.queries.scores
+SCORES_CLICKS=${DATA}/${RAWDATA}.${INDEX}.clicks.scores
+
+if [ "$MODE" == "dump" ]
+then
+    python $RERANK --verbose -k $K --coeff_queries $COEFF_QUERIES --coeff_clicks $COEFF_CLICKS --index_queries $INDEX_QUERIES --dict_queries $DICT_QUERIES --index_clicks $INDEX_CLICKS --dict_clicks $DICT_CLICKS --score_dump_queries $SCORES_QUERIES --score_dump_clicks $SCORES_CLICKS $INPUT > $RERANK_OUTPUT
+elif [ "$MODE" == "load" ]
+then
+    python $RERANK --verbose -k $K --coeff_queries $COEFF_QUERIES --coeff_clicks $COEFF_CLICKS --score_dict_queries $SCORES_QUERIES --score_dict_clicks $SCORES_CLICKS $INPUT > $RERANK_OUTPUT
+else
+    echo "Invalid mode."
+fi
+
+python $EVALUATE -k $K $RERANK_OUTPUT > $EVAL_OUTPUT
+cat $EVAL_OUTPUT
