821f3eb63270ecac775b51db2b9b5f4ca2d54a36
Charles Celerier <charles.celerier@gmail.com>
2013-05-20 23:27:54 -0700

    added try-except block to indexRead.py

--- programs/indexRead.py
+++ programs/indexRead.py
@@ -22,6 +22,9 @@ def get_posting(index_f, posting_dict, itemid):
     #return compression.dgap_decode(dgap_arr)
     #################################################
     # THE FOLLOWING IS FOR READING A COMPRESSED INDEX
-    index_f.seek(posting_dict[itemid])
-    return index_f.readline()
+    try:
+        index_f.seek(posting_dict[itemid])
+        return index_f.readline()
+    except KeyError:
+        return ""
     #################################################
