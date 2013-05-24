909dea7ed668bcfe46e78ca9eee4a06f99b4b3b4
Charles Celerier <cceleri@cs.stanford.edu>
2013-05-24 04:38:56 -0700

    front page NDCG curve for single varying parameter

--- programs/plotResults.py
+++ programs/plotResults.py
@@ -19,20 +19,28 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 smallFont = matplotlib.font_manager.FontProperties(stretch='condensed',weight='book',size='small',family='monospace')
-middleFont = matplotlib.font_manager.FontProperties(stretch='normal',weight='roman',size='large')
-titleFont = matplotlib.font_manager.FontProperties(stretch='expanded',weight='demi',size='x-large')
+middleFont = matplotlib.font_manager.FontProperties(stretch='normal',weight='roman',size='large',family='monospace')
+titleFont = matplotlib.font_manager.FontProperties(stretch='expanded',weight='demi',size='x-large',family='monospace')
+labelpad = 12
 
-def parameterInfoString(params):
+def parameterInfoString(params, minus=""):
     s = "+--------------------------+\n"\
        +"|    k = %2d  insert = %2d   |\n" % ( params["k"], params["insert_position"] )\
-       +"+------------+------+------+\n"\
-       +"|       Rank | %.2f | %.2f |\n" % ( params["coeff_rank"], params["exp_rank"] )\
-       +"|      Items | %.2f | %.2f |\n" % ( params["coeff_items"], params["exp_items"] )\
-       +"|     Clicks | %.2f | %.2f |\n" % ( params["coeff_clicks"], params["exp_clicks"] )\
-       +"|    Queries | %.2f | %.2f |\n" % ( params["coeff_queries"], params["exp_queries"] )\
-       +"|      Carts | %.2f | %.2f |\n" % ( params["coeff_carts"], params["exp_carts"] )\
-       +"| Item title | %.2f | %.2f |\n" % ( params["coeff_item_title"], params["exp_item_title"] )\
-       +"+------------+------+------+"
+       +"+------------+------+------+\n"
+    if (minus != "rank"):
+       s += "|       Rank | %.2f | %.2f |\n" % ( params["coeff_rank"], params["exp_rank"] )
+    if (minus != "items"):
+       s += "|      Items | %.2f | %.2f |\n" % ( params["coeff_items"], params["exp_items"] )
+    if (minus != "clicks"):
+       s += "|     Clicks | %.2f | %.2f |\n" % ( params["coeff_clicks"], params["exp_clicks"] )
+    if (minus != "queries"):
+       s += "|    Queries | %.2f | %.2f |\n" % ( params["coeff_queries"], params["exp_queries"] )
+    if (minus != "carts"):
+       s += "|      Carts | %.2f | %.2f |\n" % ( params["coeff_carts"], params["exp_carts"] )
+    if (minus != "item_title"):
+       s += "| Item title | %.2f | %.2f |\n" % ( params["coeff_item_title"], params["exp_item_title"] )
+
+    s += "+------------+------+------+"
 
     return s
 
@@ -50,7 +58,7 @@ def parseArgs():
 
     (options, args) = parser.parse_args()
 
-    if (len(args) != 1):
+    if (len(args) != 2):
         parser.print_usage()
         sys.exit()
 
@@ -65,26 +73,19 @@ def getLineCount(Fo):
     return i + 1
 
 def plotNDCGCurves(resultsFo):
-    numResults = getLineCount(resultsFo)
-
-    nrows  = numResults/3 + 1 if numResults % 3 else numResults / 3
-    ncols = 3
+    resultsFo.seek(0)
     
     NDCG_figure = plt.figure()
-    plt.get_current_fig_manager().resize(1600,250*nrows)
+    plt.get_current_fig_manager().resize(1600,1000)
     ax = NDCG_figure.gca()
-    ax.set_xlabel('# of results considered', labelpad=10, fontproperties=middleFont)
-    ax.set_ylabel('NDCG score', labelpad=8, fontproperties=middleFont)
+    ax.set_xlabel('# of results considered', labelpad=labelpad, fontproperties=middleFont)
+    ax.set_ylabel('NDCG score', labelpad=labelpad, fontproperties=middleFont)
 
-    row = -1 
-    col = 0
-    newLabels = ["original"]
     for i, line in enumerate(resultsFo):
         marker = matplotlib.lines.Line2D.filled_markers[i]
         dataInfo, params, stats = json.loads(line)
-        newLabels.append(parameterInfoString(params))
 
-        x = np.arange(1,len(stats['avg_orig_NDCG_scores'])+1)
+        x = range(1,len(stats['avg_orig_NDCG_scores'])+1)
 
         # plot original scores
         if (i == 0):
@@ -92,29 +93,61 @@ def plotNDCGCurves(resultsFo):
             ax.plot(x, original, '-', label='original', linewidth=5)
 
         # plot next reordered data
-        reordered = np.array(stats['avg_reordered_NDCG_scores'])
-        ax.plot(x, reordered, '-', label='reordered ' + str(i), marker=marker)
+        reordered = stats['avg_reordered_NDCG_scores']
+        ax.plot(x, reordered, '-', label=parameterInfoString(params), marker=marker)
 
-        ## red between if losing, green between if winning
-        #ax.fill_between(x, original, reordered, where=original>=reordered, facecolor='red', interpolate=True)
-        #ax.fill_between(x, original, reordered, where=original<=reordered, facecolor='green', interpolate=True)
-
-        # add the boxes on the graph
-        #addParameterBox(ax, params, 0.95, 0.12)
-
-    handles, _ = ax.get_legend_handles_labels()
-    legend = ax.legend(handles, newLabels, prop=smallFont, ncol=4, loc='lower right', fancybox=True, shadow=True)
+    legend = ax.legend(prop=smallFont, ncol=4, loc='lower right', fancybox=True, shadow=True)
     legend.draggable()
 
-    NDCG_figure.subplots_adjust(hspace=0.35,wspace=0.2,left=0.05,right=0.95,top=0.93,bottom=0.07)
     NDCG_figure.suptitle('NDCG scores for %s' % dataInfo['dataFn'].split('/')[-1], fontproperties=titleFont)
+
+def plotNDCG16Curve(resultsFo, param, which="coeff"):
+    resultsFo.seek(0)
+
+    NDCG16_figure = plt.figure()
+    plt.get_current_fig_manager().resize(1600,1000)
+    ax = NDCG16_figure.gca()
+
+    frontPageNDCGScores = []
+    for i, line in enumerate(resultsFo):
+        marker = matplotlib.lines.Line2D.filled_markers[i]
+        dataInfo, params, stats = json.loads(line)
+        
+        frontPageNDCGScores.append(
+                tuple([params[which+"_"+param],
+                    stats['avg_reordered_NDCG_scores'][15]]))
+
+    frontPageNDCGScores.sort(key=lambda a: a[0])
+
+    ax.plot([x[0] for x in frontPageNDCGScores],
+            [x[1] for x in frontPageNDCGScores],
+            'o-', label=parameterInfoString(params, minus=param),
+            linewidth=2, markersize=8)
+
+    xlabel = which+"_"+param+" = ???"
+    if (which == "coeff"):
+        xlabel += ", exp_"+param+" = "+str(params['exp_'+param])
+    else:
+        xlabel = "coeff_"+param+" = "+str(params['coeff_'+param])+", "+ xlabel
+    ax.set_xlabel(xlabel, labelpad=labelpad, fontproperties=middleFont)
+    ax.set_ylabel('front page NDCG score', labelpad=labelpad, fontproperties=middleFont)
+
+    legend = ax.legend(prop=middleFont, ncol=4, loc='best',
+                       fancybox=True, shadow=True)
+    legend.draggable()
+
+    NDCG16_figure.suptitle('Front Page NDCG Scores for %s'
+                             % dataInfo['dataFn'].split('/')[-1],
+                            fontproperties=titleFont)
 
 def main():
     (options, args) = parseArgs()
     
     resultsFo = open(args[0])
+    which, param = args[1].split('_')
     
     plotNDCGCurves(resultsFo)
+    plotNDCG16Curve(resultsFo, param, which=which)
 
     plt.show()
 
