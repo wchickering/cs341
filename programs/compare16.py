47cb77a25be62ef42e6cf42a66b2c9603df74219
Charles Celerier <cceleri@cs.stanford.edu>
2013-05-30 00:14:25 -0700

    more additions to compare16

--- programs/compare16.py
+++ programs/compare16.py
@@ -3,20 +3,30 @@ import json
 reorderedQueriesFn = "data/000050_0.48chunk.k15.reordered_queries"
 reorderedQueriesFo = open(reorderedQueriesFn)
 
-origPosCount = 0
-n = 0
 
-for line in reorderedQueriesFo:
-    lineDict = json.loads(line)
-    if (len(lineDict['reordered_shown_items']) >= 16\
-            and lineDict['reordered_shown_items'] != lineDict['shown_items']):
-        item16 = lineDict['reordered_shown_items'][15]
+for pos in range(10,20):
+    origPosCount = 0
+    n = 0
+    top = [0,0,0,0,0]
 
-        print str(item16),
-        print " was " + str(lineDict['shown_items'].index(item16)+1) + ". Now 16"
-        n += 1
-        origPosCount += lineDict['shown_items'].index(item16)+1
+    for line in reorderedQueriesFo:
+        lineDict = json.loads(line)
+        if (len(lineDict['reordered_shown_items']) >= pos\
+                and lineDict['reordered_shown_items'] != lineDict['shown_items']):
+            itempos = lineDict['reordered_shown_items'][pos-1]
+    
+            n += 1
+            origPosCount += lineDict['shown_items'].index(itempos)+1
+            for i in range(5):
+                if lineDict['shown_items'].index(itempos) < i+1:
+                    top[i] += 1
+    
+    print "===== " + str(pos) + " ====="
+    print "Number of reranked queries with >= " + str(pos) + " items: " + str(n)
+    print "Average original position of "+str(pos)+"th reranked item: " + str(float(origPosCount)/n)
+    for i in range(5):
+        print "Number of times item " + str(pos) + " was in the top " + str(i+1) +": " + str(top[i])
+    print
 
-print "Number of reranked queries with >= 16 items: " + str(n)
-print "Average original position of 16th reranked item: " + str(float(origPosCount)/n)
+    reorderedQueriesFo.seek(0)
 
