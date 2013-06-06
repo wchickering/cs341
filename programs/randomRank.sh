e06477b3656642921ddacec6a765708670612e11
Bill Chickering <chickering@cs.stanford.edu>
2013-06-06 13:43:59 -0700

    Added the -n option to reRank.py, which determines the top N items to re-rank.

--- programs/randomRank.sh
+++ programs/randomRank.sh
@@ -5,6 +5,7 @@
 # like these:
 #WORKERS=8
 #RAWDATA=000050_0
+#N=0
 #K=3
 #INSERT_POSITION=0
 
@@ -24,34 +25,35 @@ FILTERED_TEST_DATA_RANKABLE_STATS=${FILTERED_TEST_DATA_RANKABLE}.stats
 FILTERED_TEST_DATA=${TEST_DATA}.filtered
 RERANK_PROG_OUTPUT=${DATA}/${RAWDATA}.RANDOM.k${K}.reordered_queries
 EVAL_OUTPUT=${DATA}/${RAWDATA}.RANDOM.k${K}.eval
+CTR_BY_POSITION_FILE=${PROGRAMS}/${CTR_BY_POSITION}
 RERANK_LOG=${DATA}/rerank_log.txt
 
-#if [ ! -f $FILTERED_TEST_DATA ] || [ ! -f $FILTERED_TEST_DATA_RANKABLE ]
-#then
-#    echo "Filtering test data . . ."
-#    cat $TEST_DATA | python $FILTER_RANKABLE_PROG > $FILTERED_TEST_DATA_RANKABLE
-#    cat $FILTERED_TEST_DATA_RANKABLE | python $FILTER_CLICKS_PROG > $FILTERED_TEST_DATA
-#fi
-#
-#if [ ! -f $TEST_DATA_STATS ]
-#then
-#    echo "Generating test data stats . . ."
-#    python $TEST_DATA_STATS_PROG $TEST_DATA > $TEST_DATA_STATS
-#fi
-#
-#if [ ! -f $FILTERED_TEST_DATA_RANKABLE_STATS ]
-#then
-#    echo "Generating filtered test data stats . . ."
-#    python $TEST_DATA_STATS_PROG $FILTERED_TEST_DATA_RANKABLE > $FILTERED_TEST_DATA_RANKABLE_STATS
-#fi
+if [ ! -f $FILTERED_TEST_DATA ] || [ ! -f $FILTERED_TEST_DATA_RANKABLE ]
+then
+    echo "Filtering test data . . ."
+    cat $TEST_DATA | python $FILTER_RANKABLE_PROG > $FILTERED_TEST_DATA_RANKABLE
+    cat $FILTERED_TEST_DATA_RANKABLE | python $FILTER_CLICKS_PROG > $FILTERED_TEST_DATA
+fi
+
+if [ ! -f $TEST_DATA_STATS ]
+then
+    echo "Generating test data stats . . ."
+    python $TEST_DATA_STATS_PROG $TEST_DATA > $TEST_DATA_STATS
+fi
+
+if [ ! -f $FILTERED_TEST_DATA_RANKABLE_STATS ]
+then
+    echo "Generating filtered test data stats . . ."
+    python $TEST_DATA_STATS_PROG $FILTERED_TEST_DATA_RANKABLE > $FILTERED_TEST_DATA_RANKABLE_STATS
+fi
 
 echo "Randomly Reranking . . ."
-python $RERANK_PROG --workers $WORKERS --verbose -k $K --insert_position $INSERT_POSITION --random $FILTERED_TEST_DATA > $RERANK_PROG_OUTPUT
+python $RERANK_PROG --workers $WORKERS --verbose -n $N -k $K --insert_position $INSERT_POSITION --random $FILTERED_TEST_DATA > $RERANK_PROG_OUTPUT
 
-#echo "Evaluating . . ."
-#python $EVAL_PROG -k $K --test_data_fname $TEST_DATA_STATS --rankable_data_fname $FILTERED_TEST_DATA_RANKABLE_STATS $RERANK_PROG_OUTPUT > $EVAL_OUTPUT
-#echo "***********************************************************" >> $RERANK_LOG
-#echo $(date) >> $RERANK_LOG
-#echo >> $RERANK_LOG
-#cat $RERANK_PARAMS $EVAL_OUTPUT >> $RERANK_LOG
-#tail -n 15 $EVAL_OUTPUT
+echo "Evaluating . . ."
+python $EVAL_PROG -k $K --test_data_fname $TEST_DATA_STATS --rankable_data_fname $FILTERED_TEST_DATA_RANKABLE_STATS --ctr_fname $CTR_BY_POSITION_FILE $RERANK_PROG_OUTPUT > $EVAL_OUTPUT
+echo "***********************************************************" >> $RERANK_LOG
+echo $(date) >> $RERANK_LOG
+echo >> $RERANK_LOG
+cat $RERANK_PARAMS $EVAL_OUTPUT >> $RERANK_LOG
+tail -n 15 $EVAL_OUTPUT
