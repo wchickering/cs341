66c6356bbd773fad0b758a25a555493c8296cd89
Charles Celerier <cceleri@cs.stanford.edu>
2013-06-10 00:00:42 -0700

    CTR object defined. Keeps track of the click through rate as it ingests queries.

---
+++ programs/CTR.py
@@ -0,0 +1,64 @@
+import json
+
+class CTR:
+    def __init__(self):
+        self.ctf = {}
+        self.numClicks = 0.0
+
+    def ingestQuery(self, query):
+        for item in query.clicked_shown_items:
+            click_position = query.shown_items.index(item) + 1
+            try:
+                self.ctf[click_position] += 1
+            except KeyError:
+                self.ctf[click_position] = 1
+        self.numClicks += len(query.clicked_shown_items)
+
+    def CTRByPositionDict(self):
+        ctr = {}
+        for k in self.ctf.keys():
+            ctr[k] = self.ctf[k] / self.numClicks
+        return ctr
+
+    def CTFByPositionDict(self):
+        return self.ctf
+
+    def __str__(self):
+        s = ""
+        ctr = self.CTFByPositionDict()
+        for k in sorted(ctr.keys()):
+           s += str(k) + ": " + str(ctr[k]) + "  "
+        return s
+
+# custom JSON encoder for CTR object
+# http://stackoverflow.com/questions/1458450/python-serializable-objects-json
+class CTREncoder(json.JSONEncoder):
+    def default(self, obj):
+        if not isinstance(obj, CTR):
+            return super(MyEncoder, self).default(obj)
+        return obj.CTRByPositionDict()
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        print 'Usage: ' + sys.argv[0] + ' <queries>'
+        sys.exit(1)
+
+    import Query
+    from pprint import pprint
+
+    queriesFo = open(sys.argv[1])
+
+    ctrs = {}
+
+    for line in queriesFo:
+        query = Query.Query(line)
+        try:
+            ctrs[len(query)].ingestQuery(query)
+        except KeyError:
+            ctrs[len(query)] = CTR()
+            ctrs[len(query)].ingestQuery(query)
+    
+    print json.dumps(ctrs, cls=CTREncoder)
+
